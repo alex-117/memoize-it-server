57d6627afde4ba6cb06c309f92526a29099e7467
Alex Ulfich <alexulfich@gmail.com>
2021-12-29 21:00:34 -0700

    SHECS7Op: added server.js - setup express server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const PORT = 5000 || process.env.PORT;
+const app = express();
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT: ${PORT}`);
+});
